47cfb867357fd1f77623861de0d8eb18db454f9a
David-num <124700120+David-num@users.noreply.github.com>
2025-08-18 17:34:57 -0600

    Abstracción a nivel de funciones

--- poo/src/app/api/route.ts
+++ poo/src/app/api/route.ts
@@ -10,35 +10,10 @@ export async function POST(request: NextRequest) {
     const data = await request.json();
     const { title, description, author } = data;
 
-    if (typeof title !== "string") {
-      throw new Error("Invalid title format");
-    }
-
-    if (!(title.length > 0 && title.length <= 30)) {
-      throw new Error("Invalid title length");
-    }
-
-    if (typeof description !== "string") {
-      throw new Error("Invalid description format");
-    }
-
-    if (!(description.length > 0 && description.length <= 300)) {
-      throw new Error("Invalid description length");
-    }
-
-    if (typeof author !== "string") {
-      throw new Error("Invalid author format");
-    }
-
-    if (!(author.length > 0 && author.length <= 50)) {
-      throw new Error("Invalid author length");
-    }
-
-    try{
-        await sql`INSERT INTO public.post (title, description, author)VALUES (${title}, ${description}, ${author})`; 
-    }catch (error) {
-      throw new Error("Failed to save post");
-    }
+    validTitle(title);
+    validDescription(description);
+    validAuthor(author);
+    newpost({ title, description, author })
     
     return NextResponse.json(
       { message: "Data is valid" }
@@ -50,5 +25,50 @@ export async function POST(request: NextRequest) {
       { error: "Error validating data" },
       { status: 400 }
     );
+  }
+}
+
+async function newpost(data: { title: string; description: string; author: string }): Promise<void> {
+    
+    try{
+        const connectionString = "postgresql://postgres.edxdrjgaiwftvtpcyeii:laOSUDvlWF4l937H@aws-0-us-east-1.pooler.supabase.com:6543/postgres"
+        const sql = postgres(connectionString  );
+
+        await sql`INSERT INTO public.post (title, description, author)VALUES (${data.title}, ${data.description}, ${data.author})`; 
+    }catch (error) {
+      throw new Error("Failed to save post");
+    }
+}
+
+function validTitle(title: string): void {
+
+  if (typeof title !== "string") {
+    throw new Error("Invalid title format");
+  }
+
+  if (!(title.length > 0 && title.length <= 50)) {
+    throw new Error("Invalid title length");
+  }
+}
+
+function validDescription(description: string): void {
+
+  if (typeof description !== "string") {
+    throw new Error("Invalid description format");
+  }
+
+  if (!(description.length > 0 && description.length <= 300)) {
+    throw new Error("Invalid description length");
+  }
+}
+
+function validAuthor(author: string): void {
+
+  if (typeof author !== "string") {
+    throw new Error("Invalid author format");
+  }
+
+  if (!(author.length > 0 && author.length <= 30)) {
+    throw new Error("Invalid author length");
   }
 }
